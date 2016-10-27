cb39b171f5724cd453b66fb53e85e69cc67b0150
Cameron Hunter <chunter@netflix.com>
2016-10-26 23:16:37 -0700

    Initial implementation

--- src/chuck-norris.js
+++ src/chuck-norris.js
@@ -1,28 +1,44 @@
 import { Skill, Launch, Intent } from 'alexa-annotations';
-import { say, ask } from 'alexa-response';
+import Response from 'alexa-response';
 import ssml from 'alexa-ssml-jsx';
+import fetch from 'isomorphic-fetch';
+
+const API_ENDPOINT = 'http://api.icndb.com/jokes/random?escape=javascript';
 
 @Skill
 export default class ChuckNorris {
 
   @Launch
-  launch() {
-    return say('Chuck Norris launched!');
-  }
-
-  @Intent('hello')
-  hello({ name = 'world' }) {
-    return say(`Hello ${name}`).card({ title: 'Chuck Norris', content: `Hello ${name}` });
+  @Intent('AMAZON.YesIntent', 'joke')
+  joke() {
+    return this.getJoke().then(joke => Response.build({
+      ask: (
+        <speak>
+          <s>{joke}</s>
+          <break time='1s' />
+          <s>Would you like to hear another?</s>
+        </speak>
+      ),
+      reprompt: 'Would you like to hear another?',
+      card: {
+        title: 'Chuck Norris',
+        content: joke
+      }
+    }));
   }
 
   @Intent('AMAZON.HelpIntent')
   help() {
-    return ask('I say hello to people. Who should I say hello to?').reprompt('Who should I say hello to?');
+    return Response.build({
+      ask: 'I tell jokes about Chuck Norris. Would you like to hear one?',
+      reprompt: 'Would you like to hear a Chuck Norris joke?'
+    });
   }
 
-  @Intent('AMAZON.CancelIntent', 'AMAZON.StopIntent')
-  stop() {
-    return say(<speak>Goodbye!</speak>);
+  getJoke() {
+    return fetch(API_ENDPOINT).then(_ => _.json()).then(response => {
+      const { type, value } = response;
+      return type === 'success' ? value.joke : Promise.reject();
+    });
   }
-
 }
--- test/index-test.js
+++ test/index-test.js
@@ -4,29 +4,10 @@ import Request from 'alexa-request';
 
 test('LaunchRequest', t => {
   const event = Request.launchRequest().build();
-
-  return Skill(event).then(response => {
-    t.deepEqual(response, {
-      version: '1.0',
-      response: {
-        shouldEndSession: true,
-        outputSpeech: { type: 'PlainText', text: 'Chuck Norris launched!' }
-      }
-    });
-  });
+  return Skill(event).then(response => t.truthy(response));
 });
 
 test('Hello intent', t => {
   const event = Request.intent('hello', { name: 'world' }).build();
-
-  return Skill(event).then(response => {
-    t.deepEqual(response, {
-      version: '1.0',
-      response: {
-        shouldEndSession: true,
-        outputSpeech: { type: 'PlainText', text: 'Hello world' },
-        card: { type: 'Simple', title: 'Chuck Norris', content: 'Hello world' }
-      }
-    });
-  });
+  return Skill(event).then(response => t.truthy(response));
 });
